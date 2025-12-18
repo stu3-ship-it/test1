3505ba0920a67ef4a38ad3672b31b36d959809c9
stu3-ship-it <stu3@go.pymhs.tyc.edu.tw>
2025-12-18 10:16:20 +0800

    Update ai_studio_code.py

--- ai_studio_code.py
+++ ai_studio_code.py
@@ -18,10 +18,10 @@ from datetime import datetime, date, timedelta
 from datetime import timezone
 import pytz
 #import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-from googleapiclient.discovery import build
-from googleapiclient.http import MediaIoBaseUpload
-from streamlit.runtime.scriptrunner import add_script_run_ctx
+#from oauth2client.service_account import ServiceAccountCredentials
+#from googleapiclient.discovery import build
+#from googleapiclient.http import MediaIoBaseUpload
+#from streamlit.runtime.scriptrunner import add_script_run_ctx
 
 # --- 1. 網頁設定 ---
 st.set_page_config(page_title="抄襲是不對的行為", layout="wide", page_icon="🌞")
