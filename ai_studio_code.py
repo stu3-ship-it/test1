33dddbc0d80d2f2d1b224092aa1e01dc5cdbebcd
stu3-ship-it <stu3@go.pymhs.tyc.edu.tw>
2026-01-09 13:27:01 +0800

    Update ai_studio_code.py

--- ai_studio_code.py
+++ ai_studio_code.py
@@ -1204,8 +1204,7 @@ try:
                             
                             is_fix = st.checkbox("🚩 這是修正單 (上一筆輸錯，這筆重key才要按)", key=f"fix_{selected_class}")
                             files = st.file_uploader("📸 違規照片 (若有扣分則必填)", accept_multiple_files=True, key=f"file_{selected_class}")
-                            print(files)
-                            
+                            st.write(files)
                             st.write("") 
 
                             if st.form_submit_button("🚀 送出評分", width="stretch"):
