9c1d03ddceefbc25c2771b7774d55f076c9c9e9c
stu3-ship-it <stu3@go.pymhs.tyc.edu.tw>
2026-01-21 14:17:09 +0800

    Update ai_studio_code.py

--- ai_studio_code.py
+++ ai_studio_code.py
@@ -376,8 +376,7 @@ try:
                         continue
                     with open(path, "rb") as f:
                         link = upload_image_to_drive(f, fname)
-                    #drive_links.append(link if link else "UPLOAD_FAILED_379")
-                    drive_links=link
+                    drive_links.append(link if link else "UPLOAD_FAILED_379")
 
                 if drive_links:
                     entry["照片路徑"] = ";".join(drive_links)
